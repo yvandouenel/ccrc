338cfcc0650987cbd1bd7d2f42f5abbc7609547b
Yvan Douënel <yvan@expert-web.org>
2013-01-16 12:07:38 +0100

    page.tpl.php modified to put h2 on title and to display nothing on homepage + css + flash

--- sites/all/themes/ew3-dagobert/templates/page.tpl.php
+++ sites/all/themes/ew3-dagobert/templates/page.tpl.php
@@ -111,8 +111,8 @@
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+      <?php if ($title &&  !$is_front ): ?>
+        <h2 class="title" id="page-title"><?php print $title; ?></h2>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
